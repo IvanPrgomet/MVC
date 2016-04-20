4008b237e47d1684a642310ca374b1207af1fa90
IvanPrgomet <ivan.prgomet@telia.com>
2016-04-20 11:04:33 +0200

    Skapat projekt

---
+++ Laborationer/Labb1/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Labb1.Startup))]
+namespace Labb1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            //Ivan Prgomet
+        }
+    }
+}
