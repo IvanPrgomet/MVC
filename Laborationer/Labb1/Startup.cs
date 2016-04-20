da0f59635424fc71e33cdf9e57b786bcb53c0dd1
IvanPrgomet <ivan.prgomet@telia.com>
2016-04-20 11:05:21 +0200

    Funktionstest

--- Laborationer/Labb1/Startup.cs
+++ Laborationer/Labb1/Startup.cs
@@ -9,7 +9,7 @@ namespace Labb1
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            //Ivan Prgomet
+            //Ivan Prgomet 1
         }
     }
 }
