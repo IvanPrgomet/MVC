7c1f589d961af92987ca87d5b2cfc973820a35d9
IvanPrgomet <ivan.prgomet@telia.com>
2016-05-03 10:02:17 +0200

    Lab 2 - Spel Tjugo Ett klart

---
+++ Laborationer/Labb2/Models/TjugoEttModels.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+
+namespace Labb2.Models
+{
+    public class TjugoEttModels
+    {
+        public int CpuTurn { get; set; }
+        public int Increment { get; set; }
+        public int ActualNumber { get; set; }
+        public string GameOverText { get; set; }
+        public int Randomize(int min, int max)
+        {
+            Random random = new Random();
+            int result = random.Next(min, max);
+            return result;
+        }
+
+        public void ComputerChoice()
+        {
+            int cpuChoice = 0;
+            switch (ActualNumber)
+            {
+                case 19:
+                    cpuChoice = 2;
+                    break;
+                case 20:
+                    cpuChoice = 1;
+                    break;
+                default:
+                    if (CpuTurn%2 == 1)
+                    {
+                        cpuChoice = CpuSmartMove(cpuChoice);
+                    }
+                    else
+                    {
+                        cpuChoice = Randomize(1, 3);
+                    }
+                    break;
+            }
+            Increment = cpuChoice;
+            CpuTurn++;
+        }
+
+    public int CpuSmartMove(int cpuChoice)
+        {
+            switch (ActualNumber%3)
+            {
+                case 1:
+                    cpuChoice = 2;
+                    break;
+                case 2:
+                    cpuChoice = 1;
+                    break;
+                case 0:
+                    cpuChoice  = Randomize(1, 3);
+                    break;
+            }
+            return cpuChoice;
+        }
+
+        public void IncreaseActualNumber(int i)
+        {
+            if (ActualNumber < 21)
+            {
+                ActualNumber += Increment;
+                if (ActualNumber == 21)
+                {
+                    if (i == 1)
+                    {
+                        GameOverText = "You win!";
+                    }
+                    else if (i == 2)
+                    {
+                        GameOverText = "You loose!";
+                    }
+                }
+            }
+        }
+    }
+}
